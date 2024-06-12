9ebac4465a204992b123383280152a414f9bc169
guerrerojonathan003 <guerrerojonathan003@gmail.com>
2024-06-12 23:53:15 +0000

    mensaje bacano

---
+++ papucho/main.js
@@ -0,0 +1,9 @@
+console.log("Hola")
+console.log("Bienvenidos a la materia de Principios de Programación Lógica")
+
+/*
+pasos para mandar camcios a repositorio desde terminal
+1.- git add .
+2.- git commit -m "mensage de lo que mandas al repo"
+3.- git push origin master
+*/
